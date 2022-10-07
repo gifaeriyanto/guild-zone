f6ac983639fbef8e35ca2753164cc9dbeb1cb63b
Gifa Eriyanto <gifa.eriyanto@gmail.com>
2022-10-08 03:39:03 +0800

    initial commit

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  // your config
+
+  // webpack config
+  webpack(config, _options) {
+    return config;
+  },
+};
+
+module.exports = nextConfig;
