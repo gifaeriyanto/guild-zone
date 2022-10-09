9ac4076ae0aa1e472013d8b8552268941eabab12
Gifa Eriyanto <gifa.eriyanto@gmail.com>
2022-10-09 14:30:58 +0800

    feat(theme): add montserrat font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -16,7 +16,18 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="anonymous"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700;800;900&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
