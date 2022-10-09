6c601efd4657b7fa813c73b9226a84a192fea4ac
Gifa Eriyanto <gifa.eriyanto@gmail.com>
2022-10-10 01:16:29 +0800

    feat(layout): update main layout

--- app/layout/main.tsx
+++ app/layout/main.tsx
@@ -38,13 +38,14 @@ export const MainLayout: React.FC<PropsWithChildren> = ({ children }) => {
 
   return (
     <>
-      <Container maxW="container.lg">
-        <Flex py={8} justify="space-between">
+      <Container maxW="container.lg" mb="8rem">
+        <Flex py={8} justify="space-between" mb={8}>
           <Box>
             <Link href="/">
               <Img
                 src="https://assets.website-files.com/62d867fa7c6c5d5faaf26ad3/62dc01ef48e9488670b72569_logo2.svg"
                 alt="Blockchain Space Logo"
+                cursor="pointer"
               />
             </Link>
           </Box>
